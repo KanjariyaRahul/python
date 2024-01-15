08535203336f1b36eaef15497ca30c03bd411a54
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2024-01-15 09:12:21 +0530

    Add files via upload

---
+++ Practice Set-2.py
@@ -0,0 +1,49 @@
+#  1) write a python program to find area of triangle
+
+b = int(input("Input the base : "))
+h = int(input("Input the height : "))
+
+
+area = b * h / 2
+
+print("area = ", area)
+
+
+# 2) write a python program to find area of square
+
+
+s=float(input("Enter side of square : " ))
+
+area=s*s
+
+print("Area of square=", area)
+
+
+# 3) write a python program to calculate celsius to fahrenheit
+
+celsius=float(input("Enter  of Celsius: " ))
+
+fahrenheit = (celsius  * 9/5) + 32
+
+print("calculate celsius to fahrenheit = ", fahrenheit)
+
+
+# 4) write a python program to convert us dollars to indian rupees
+
+dollars = float(input("Please enter dollars:"))
+rupees = dollars * 82.88
+
+print('convert us dollars to indian rupees',  rupees)
+
+
+# 5) write a python program to convert litres to millilitres
+
+litres = float(input("Enter the volume in litres: "))
+
+
+millilitres = litres * 1000
+
+print('convert liters to millilitres', millilitres)
+
+
+ 
