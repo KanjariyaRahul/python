0bd4e3ed1a109d5273b8eddb954fc400c91591c1
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2024-02-11 16:22:19 +0530

    Add files via upload

---
+++ Practice Set-3/script_4.py
@@ -0,0 +1,18 @@
+
+marks = float(input("Enter the marks scored by the student: "))
+
+if marks > 90:
+    print("A1 Grade")
+elif marks <= 90 and marks > 80:
+    print("A Grade")
+elif marks <= 80 and marks > 70:
+    print("B1 Grade")
+    
+elif marks <= 70 and marks > 60:
+    print("B Grade")
+    
+elif marks <= 60 and marks > 50:
+    print("Can do Better!")
+else:
+    print("Need to work hard.")
+
